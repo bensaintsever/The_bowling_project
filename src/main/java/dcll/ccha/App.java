af9768bc84196e82d9bc04601c491355f008042a
guillouthomas <guillou.thomas.pro@gmail.com>
2016-03-07 08:52:47 +0100

    Ajout des dependances

---
+++ src/main/java/dcll/ccha/App.java
@@ -0,0 +1,13 @@
+package dcll.ccha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
