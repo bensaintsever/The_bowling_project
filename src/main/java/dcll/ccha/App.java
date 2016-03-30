4263c085fad25e2a57c488a1e772651e019035d6
guillouthomas <guillou.thomas.pro@gmail.com>
2016-03-30 20:44:09 +0200

    The_bowling_project-15 CheckStyle couverture 0 erreurs

--- src/main/java/dcll/ccha/App.java
+++ src/main/java/dcll/ccha/App.java
@@ -5,8 +5,21 @@ import joueur.Joueur;
 /**
  * Classe de lancement.
  */
-public class App {
-    public static void main(String[] args) {
+public final class App {
+
+    /**
+     * Méthode main de l'application.
+     * @param args paramètres donnés lors du lancement de l'application.
+     */
+    public static void main(final String[] args) {
         new Joueur("Tom");
+    }
+
+    /**
+     * Constructeur de la classe App pour que checkstyle soit content.
+     * Le constructeur est privé car instancier la classe App n'a pas de sens :
+     * En effet celle-çi ne contient que la méthode main (static)
+     */
+    private App() {
     }
 }
